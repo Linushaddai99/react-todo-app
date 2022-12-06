86b706f7d17de8a3d2819a4a036985470b14ed50
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2022-12-06 20:56:45 +0100

    todo app functionality

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom"
+//stylesheet
+import "./functionBased/App.css"
+import { BrowserRouter as Router } from "react-router-dom"
+
+import TodoContainer from "./functionBased/components/TodoContainer"
+
+ReactDOM.render(
+    <React.StrictMode>
+         <Router>
+            <TodoContainer />
+        </Router>
+    </React.StrictMode>
+    , document.getElementById("root")
+)
